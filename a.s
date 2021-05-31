03be76edf0b63f05cb03a39608ff7c7d74be91df
Ethan Nguyen <ethandn1@uci.edu>
2021-05-31 11:41:32 -0700

    Updated: CodeGen.java

--- a.s
+++ a.s
@@ -1,1 +1,469 @@
-    .comm a, 8, 8
+    .globl _garble
+_garble:
+    enter $(8 * 90), $0
+    movq %rdi, -8(%rbp)
+    movq %rsi, -16(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -24(%rbp)
+    /*** CopyInst ***/
+    movq -24(%rbp), %r10
+    movq %r10, -32(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -40(%rbp)
+    /*** CopyInst ***/
+    movq -40(%rbp), %r10
+    movq %r10, -48(%rbp)
+L9:
+    /*** CopyInst ***/
+    movq -48(%rbp), %r10
+    movq %r10, -56(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -64(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -56(%rbp), %r11
+    cmp -64(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -72(%rbp)
+    /*** CopyInst ***/
+    movq %r10, -80(%rbp)
+    /*** JumpInst ***/
+    movq -80(%rbp), %r10
+    cmp $1, %r10
+    je L1
+L2:
+    /*** JumpInst ***/
+    movq -80(%rbp), %r10
+    cmp $1, %r10
+    je L3
+    /*** CopyInst ***/
+    movq -80(%rbp), %r10
+    movq %r10, -88(%rbp)
+    /*** CopyInst ***/
+    movq -88(%rbp), %r10
+    movq %r10, -96(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -104(%rbp)
+    /*** BinaryOperator ***/
+    movq -96(%rbp), %rax
+    cqto
+    idivq -104(%rbp)
+    movq %rax, -112(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -120(%rbp)
+    /*** BinaryOperator ***/
+    movq -112(%rbp), %r10
+    imulq -120(%rbp), %r10
+    movq %r10, -128(%rbp)
+    /*** BinaryOperator ***/
+    movq -88(%rbp), %r10
+    subq -128(%rbp), %r10
+    movq %r10, -136(%rbp)
+    /*** CopyInst ***/
+    movq -136(%rbp), %r10
+    movq %r10, -144(%rbp)
+    /*** CopyInst ***/
+    movq -144(%rbp), %r10
+    movq %r10, -152(%rbp)
+    /*** CopyInst ***/
+    movq -152(%rbp), %r10
+    movq %r10, -160(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -168(%rbp)
+    /*** BinaryOperator ***/
+    movq -160(%rbp), %rax
+    cqto
+    idivq -168(%rbp)
+    movq %rax, -176(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -184(%rbp)
+    /*** BinaryOperator ***/
+    movq -176(%rbp), %r10
+    imulq -184(%rbp), %r10
+    movq %r10, -192(%rbp)
+    /*** BinaryOperator ***/
+    movq -152(%rbp), %r10
+    subq -192(%rbp), %r10
+    movq %r10, -200(%rbp)
+    /*** CopyInst ***/
+    movq -200(%rbp), %r10
+    movq %r10, -208(%rbp)
+    /*** CopyInst ***/
+    movq -208(%rbp), %r10
+    movq %r10, -216(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -224(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -216(%rbp), %r11
+    cmp -224(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -232(%rbp)
+    /*** CopyInst ***/
+    movq %r10, -240(%rbp)
+    /*** JumpInst ***/
+    movq -240(%rbp), %r10
+    cmp $1, %r10
+    je L6
+L7:
+    /*** JumpInst ***/
+    movq -240(%rbp), %r10
+    cmp $1, %r10
+    je L8
+    /*** CopyInst ***/
+    movq -240(%rbp), %r10
+    movq %r10, -248(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -256(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -248(%rbp), %r11
+    cmp -256(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -264(%rbp)
+    /*** CopyInst ***/
+    movq %r10, -272(%rbp)
+    /*** JumpInst ***/
+    movq -272(%rbp), %r10
+    cmp $1, %r10
+    je L10
+L11:
+    /*** JumpInst ***/
+    movq -272(%rbp), %r10
+    cmp $1, %r10
+    je L12
+    /*** CopyInst ***/
+    movq -272(%rbp), %r10
+    movq %r10, -280(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -288(%rbp)
+    /*** BinaryOperator ***/
+    movq -280(%rbp), %r10
+    imulq -288(%rbp), %r10
+    movq %r10, -296(%rbp)
+    /*** CopyInst ***/
+    movq -296(%rbp), %r10
+    movq %r10, -32(%rbp)
+L14:
+L13:
+    /*** CopyInst ***/
+    movq -296(%rbp), %r10
+    movq %r10, -304(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -312(%rbp)
+    /*** BinaryOperator ***/
+    movq -304(%rbp), %rax
+    cqto
+    idivq -312(%rbp)
+    movq %rax, -320(%rbp)
+    /*** CopyInst ***/
+    movq -320(%rbp), %r10
+    movq %r10, -328(%rbp)
+    /*** CopyInst ***/
+    movq -328(%rbp), %r10
+    movq %r10, -336(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -344(%rbp)
+    /*** BinaryOperator ***/
+    movq -336(%rbp), %rax
+    cqto
+    idivq -344(%rbp)
+    movq %rax, -352(%rbp)
+    /*** CopyInst ***/
+    movq -352(%rbp), %r10
+    movq %r10, -360(%rbp)
+    /*** CopyInst ***/
+    movq -360(%rbp), %r10
+    movq %r10, -368(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -376(%rbp)
+    /*** BinaryOperator ***/
+    movq -368(%rbp), %r10
+    addq -376(%rbp), %r10
+    movq %r10, -384(%rbp)
+    /*** CopyInst ***/
+    movq -384(%rbp), %r10
+    movq %r10, -48(%rbp)
+L12:
+    /*** CopyInst ***/
+    movq -384(%rbp), %r10
+    movq %r10, -392(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -400(%rbp)
+    /*** BinaryOperator ***/
+    movq -392(%rbp), %r10
+    imulq -400(%rbp), %r10
+    movq %r10, -408(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -416(%rbp)
+    /*** BinaryOperator ***/
+    movq -408(%rbp), %r10
+    addq -416(%rbp), %r10
+    movq %r10, -424(%rbp)
+    /*** CopyInst ***/
+    movq -424(%rbp), %r10
+    movq %r10, -32(%rbp)
+L10:
+    /*** CopyInst ***/
+    movq -424(%rbp), %r10
+    movq %r10, -432(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -440(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -432(%rbp), %r11
+    cmp -440(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -448(%rbp)
+    /*** CopyInst ***/
+    movq %r10, -272(%rbp)
+L8:
+    /*** CopyInst ***/
+    movq -448(%rbp), %r10
+    movq %r10, -456(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -464(%rbp)
+    /*** BinaryOperator ***/
+    movq -456(%rbp), %r10
+    imulq -464(%rbp), %r10
+    movq %r10, -472(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -480(%rbp)
+    /*** BinaryOperator ***/
+    movq -472(%rbp), %r10
+    addq -480(%rbp), %r10
+    movq %r10, -488(%rbp)
+    /*** CopyInst ***/
+    movq -488(%rbp), %r10
+    movq %r10, -32(%rbp)
+L6:
+    /*** CopyInst ***/
+    movq -488(%rbp), %r10
+    movq %r10, -496(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -504(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -496(%rbp), %r11
+    cmp -504(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -512(%rbp)
+    /*** CopyInst ***/
+    movq %r10, -240(%rbp)
+L3:
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -520(%rbp)
+    /*** CopyInst ***/
+    movq -520(%rbp), %r10
+    movq %r10, -528(%rbp)
+L5:
+    /*** CopyInst ***/
+    movq -528(%rbp), %r10
+    movq %r10, -536(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -544(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -536(%rbp), %r11
+    cmp -544(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -552(%rbp)
+    /*** JumpInst ***/
+    movq -552(%rbp), %r10
+    cmp $1, %r10
+    je L4
+    /*** CopyInst ***/
+    movq -552(%rbp), %r10
+    movq %r10, -560(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -568(%rbp)
+    /*** BinaryOperator ***/
+    movq -560(%rbp), %r10
+    imulq -568(%rbp), %r10
+    movq %r10, -576(%rbp)
+    /*** CopyInst ***/
+    movq -576(%rbp), %r10
+    movq %r10, -584(%rbp)
+    /*** CopyInst ***/
+    movq -584(%rbp), %r10
+    movq %r10, -592(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -600(%rbp)
+    /*** BinaryOperator ***/
+    movq -592(%rbp), %rax
+    cqto
+    idivq -600(%rbp)
+    movq %rax, -608(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -616(%rbp)
+    /*** BinaryOperator ***/
+    movq -608(%rbp), %r10
+    imulq -616(%rbp), %r10
+    movq %r10, -624(%rbp)
+    /*** BinaryOperator ***/
+    movq -584(%rbp), %r10
+    subq -624(%rbp), %r10
+    movq %r10, -632(%rbp)
+    /*** BinaryOperator ***/
+    movq -576(%rbp), %r10
+    addq -632(%rbp), %r10
+    movq %r10, -640(%rbp)
+    /*** CopyInst ***/
+    movq -640(%rbp), %r10
+    movq %r10, -528(%rbp)
+    /*** CopyInst ***/
+    movq -640(%rbp), %r10
+    movq %r10, -648(%rbp)
+    /*** CopyInst ***/
+    movq $2, %r10
+    movq %r10, -656(%rbp)
+    /*** BinaryOperator ***/
+    movq -648(%rbp), %rax
+    cqto
+    idivq -656(%rbp)
+    movq %rax, -664(%rbp)
+    /*** CopyInst ***/
+    movq -664(%rbp), %r10
+    movq %r10, -32(%rbp)
+    /*** CopyInst ***/
+    movq -664(%rbp), %r10
+    movq %r10, -672(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -680(%rbp)
+    /*** BinaryOperator ***/
+    movq -672(%rbp), %r10
+    subq -680(%rbp), %r10
+    movq %r10, -688(%rbp)
+    /*** CopyInst ***/
+    movq -688(%rbp), %r10
+    movq %r10, -48(%rbp)
+L4:
+    /*** CopyInst ***/
+    movq -688(%rbp), %r10
+    movq %r10, -696(%rbp)
+    /*** ReturnInst ***/
+    movq -696(%rbp), %rax
+    leave
+    ret
+L1:
+    /*** CopyInst ***/
+    movq -696(%rbp), %r10
+    movq %r10, -704(%rbp)
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -712(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -704(%rbp), %r11
+    cmp -712(%rbp), %r11
+    cmove %rax, %r10
+    movq %r10, -720(%rbp)
+    /*** CopyInst ***/
+    movq %r10, -80(%rbp)
+    jmp L4
+    leave
+    ret
+    .globl _main
+_main:
+    enter $(8 * 106), $0
+    /*** CopyInst ***/
+    movq $0, %r10
+    movq %r10, -728(%rbp)
+    /*** CopyInst ***/
+    movq -728(%rbp), %r10
+    movq %r10, -736(%rbp)
+L16:
+    /*** CopyInst ***/
+    movq -736(%rbp), %r10
+    movq %r10, -744(%rbp)
+    /*** CopyInst ***/
+    movq $40, %r10
+    movq %r10, -752(%rbp)
+    /*** CompareInst ***/
+    movq $0, %r10
+    movq $1, %rax
+    movq -744(%rbp), %r11
+    cmp -752(%rbp), %r11
+    cmovge %rax, %r10
+    movq %r10, -760(%rbp)
+    /*** JumpInst ***/
+    movq -760(%rbp), %r10
+    cmp $1, %r10
+    je L15
+    /*** CallInst ***/
+    call _readChar
+    movq %rax, -768(%rbp)
+    /*** CopyInst ***/
+    movq -768(%rbp), %r10
+    movq %r10, -776(%rbp)
+    /*** CopyInst ***/
+    movq -776(%rbp), %r10
+    movq %r10, -784(%rbp)
+    /*** CopyInst ***/
+    movq $7, %r10
+    movq %r10, -792(%rbp)
+    /*** CopyInst ***/
+    movq -792(%rbp), %r10
+    movq %r10, -800(%rbp)
+    /*** BinaryOperator ***/
+    movq -792(%rbp), %r10
+    addq -800(%rbp), %r10
+    movq %r10, -808(%rbp)
+    /*** CallInst ***/
+    movq -784(%rbp), %rdi
+    movq -808(%rbp), %rsi
+    call _garble
+    movq %rax, -816(%rbp)
+    /*** CallInst ***/
+    movq -816(%rbp), %rdi
+    call _printChar
+    movq %rax, -824(%rbp)
+    /*** CopyInst ***/
+    movq -824(%rbp), %r10
+    movq %r10, -832(%rbp)
+    /*** CopyInst ***/
+    movq $1, %r10
+    movq %r10, -840(%rbp)
+    /*** BinaryOperator ***/
+    movq -832(%rbp), %r10
+    addq -840(%rbp), %r10
+    movq %r10, -848(%rbp)
+    /*** CopyInst ***/
+    movq -848(%rbp), %r10
+    movq %r10, -736(%rbp)
+    leave
+    ret
+L15:
+    jmp L16
--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -1,6 +1,8 @@
 package crux.backend;
 
 import crux.frontend.Symbol;
+import crux.frontend.types.ArrayType;
+import crux.frontend.types.BoolType;
 import crux.frontend.types.IntType;
 import crux.midend.ir.core.*;
 import crux.midend.ir.core.insts.*;
@@ -40,26 +42,38 @@ public final class CodeGen extends InstVisitor {
     for (Iterator<Function> functIt = p.getFunctions(); functIt.hasNext();) {
       Function f = functIt.next();
       genCode(f);
+
+      if (functIt.hasNext()){
+        out.outputBuffer();
+        out.printCode("leave");
+        out.printCode("ret");
+      }
+
     }
 
     out.outputBuffer();
-    out.printCode("movq $0, %rax");
-    out.printCode("leave");
-    out.printCode("ret");
-
+    if (leaveRet == 0){
+      out.printCode("leave");
+      out.printCode("ret");
+    }
     out.close();
   }
 
   private int labelcount = 1;
 
-  // ADDED //////////
+  // ADDED - START //////////
+  int leaveRet = 0;
+
+  Stack<Instruction> dfsStack = new Stack<>();
+  HashSet<Instruction> visited = new HashSet<>();
+
+  HashMap<Instruction, String> currlabelMap = new HashMap<>();
 
   //argRegisters is a global array that holds the 6 registers for arguments
   String[] argRegisters = new String[]{"%rdi","%rsi","%rdx","%rcx","%r8","%r9"};
 
   private int stackCount = 0;
   HashMap<Variable, Integer> varMap = new HashMap<Variable, Integer>();
-
   Integer getStackSlots(Variable v) {
     if (!varMap.containsKey(v)) {
       stackCount++;
@@ -68,7 +82,7 @@ public final class CodeGen extends InstVisitor {
 
     return varMap.get(v);
   }
-  // ADDED END /////////
+  // ADDED - END ////////////
 
 
   private String getNewLabel() {
@@ -77,15 +91,63 @@ public final class CodeGen extends InstVisitor {
 
   private void genCode(Function f) {
     Instruction inst = f.getStart();
-    while (inst != null){
-      inst.accept(this);
-      inst = inst.getNext(0);
+    if (inst.getNext(0) != null){
+      currlabelMap = assignLabels(f);
     }
 
-    if (f.getName().equals("main")) {
-      out.printCode(".globl _" + f.getName());
+
+    int i = 0;
+    for (var e: f.getArguments()) {
+      ++stackCount;
+      if (i < 6) {
+        out.bufferCode("movq " + argRegisters[i] + ", " + (-8 * stackCount) + "(%rbp)");
+      }
+      ++i;
     }
 
+//    while (inst != null){
+//      inst.accept(this);
+//      inst = inst.getNext(0);
+//    }
+
+
+    //Depth-First-Search
+    dfsStack.push(inst);
+    String lbl = "";
+    while (!dfsStack.isEmpty() && inst.getNext(0) != null){
+      Instruction currInst = dfsStack.pop();
+      visited.add(currInst);
+
+      if (currlabelMap.containsKey(currInst)){
+
+        if (currlabelMap.size() == 1) {
+          out.bufferCode("leave");
+          out.bufferCode("ret");
+          ++leaveRet;
+        }
+
+        String jmplabel = currlabelMap.get(currInst);
+        out.bufferLabel(jmplabel + ":");
+        if (currlabelMap.size() == 2){
+          lbl = jmplabel;
+        }
+        currlabelMap.remove(currInst, jmplabel);
+      }
+
+      currInst.accept(this);
+      if (currInst.numNext() != 0){
+        Instruction next = currInst.getNext(0);
+        if (!visited.contains(next)){
+          dfsStack.push(next);
+        }
+      }
+    }
+    if (!lbl.equals("")){
+      out.bufferCode("jmp " + lbl);
+    }
+
+
+    out.printCode(".globl _" + f.getName());
     out.printLabel("_" + f.getName() + ":");
     int numSlots = stackCount;
     numSlots = (numSlots + 1) &  ~1;
@@ -133,8 +195,22 @@ public final class CodeGen extends InstVisitor {
     Symbol base = i.getBase();
     LocalVar offset = i.getOffset();
 
-//    int dstOffset = getStackSlots(dst);
-
+    if (base.getType().getClass() == ArrayType.class) {
+      out.bufferCode("movq " + (-8 * stackCount) + "(%rbp), %r11");
+//      int offsetVar = getStackSlots(offset);
+      out.bufferCode("movq $8, %r10");  //8 or offset ???
+      out.bufferCode("imul %r10, %r11");
+      out.bufferCode("movq " + base.getName() + "@GOTPCREL(%rip), %r10");
+      out.bufferCode("addq %r10, %r11");
+      int dstSlot = getStackSlots(dst);
+      int dstOffset = -8 * dstSlot;
+      out.bufferCode("movq %r11, " + dstOffset + "(%rbp)");
+    } else {
+      out.bufferCode("movq " + base.getName() + "@GOTPCREL(%rip), %r10");
+      int dstSlot = getStackSlots(dst);
+      int dstOffset = -8 * dstSlot;
+      out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+    }
 
   }
 
@@ -179,48 +255,151 @@ public final class CodeGen extends InstVisitor {
   }
 
   public void visit(CompareInst i) {
+    out.bufferCode("/*** CompareInst ***/");
+    LocalVar lhs = i.getLeftOperand();
+    LocalVar rhs = i.getRightOperand();
+    LocalVar dst = i.getDst();
+
+    int dstSlot = getStackSlots(dst);
+    int lhsSlot = getStackSlots(lhs);
+    int rhsSlot = getStackSlots(rhs);
+
+    int dstOffset = -8 * dstSlot;
+    int lhsOffset = -8 * lhsSlot;
+    int rhsOffset = -8 * rhsSlot;
+
+    out.bufferCode("movq $0, %r10");
+    out.bufferCode("movq $1, %rax");
+    out.bufferCode("movq " + lhsOffset + "(%rbp), %r11");
+    out.bufferCode("cmp " + rhsOffset + "(%rbp), %r11");
+    switch(i.getPredicate()) {
+      case EQ:
+        out.bufferCode("cmove %rax, %r10");
+        out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+        break;
+      case GE:
+        out.bufferCode("cmovge %rax, %r10");
+        out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+        break;
+      case GT:
+        out.bufferCode("cmovg %rax, %r10");
+        out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+        break;
+      case LE:
+        out.bufferCode("cmovle %rax, %r10");
+        out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+        break;
+      case LT:
+        out.bufferCode("cmovl %rax, %r10");
+        out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+        break;
+      case NE:
+        out.bufferCode("cmovne %rax, %r10");
+        out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+        break;
+    }
   }
 
   public void visit(CopyInst i) {
     out.bufferCode("/*** CopyInst ***/");
     LocalVar dst = i.getDstVar();
     Value val = i.getSrcValue();
-    //out.bufferCode("// " + dst + " = " + val);
-    int slotNum = getStackSlots(dst);
-    int offset = -8 * slotNum;
 
     if (val.getType().equivalent(new IntType())) {
-      long value = ((IntegerConstant) val).getValue();
-      out.bufferCode("movq $" + value + ", %r10");
-    } else {
-      boolean value = ((BooleanConstant) val).getValue();
-      if (value){
-        out.bufferCode("movq $" + 1 + ", %r10");
+      if (val.getClass().equals(IntegerConstant.class)) {
+        long value = ((IntegerConstant) val).getValue();
+        out.bufferCode("movq $" + value + ", %r10");
       } else {
-        out.bufferCode("movq $" + 0 + ", %r10");
+        out.bufferCode("movq " + (-8 * stackCount) + "(%rbp), %r10");
+      }
+
+    } else {
+      if (val.getClass().equals(BooleanConstant.class)) {
+        boolean value = ((BooleanConstant) val).getValue();
+
+        if (value){
+          out.bufferCode("movq $" + 1 + ", %r10");
+        } else {
+          out.bufferCode("movq $" + 0 + ", %r10");
+        }
+
       }
 
     }
 
+    int slotNum = getStackSlots(dst);
+    int offset = -8 * slotNum;
     out.bufferCode("movq %r10, " + offset + "(%rbp)");
   }
 
-  public void visit(JumpInst i) {}
+  public void visit(JumpInst i) {
+    out.bufferCode("/*** JumpInst ***/");
+    out.bufferCode("movq " + (-8 * stackCount) +  "(%rbp), %r10");
 
-  public void visit(LoadInst i) {}
+    if (i.getPredicate().getType().equivalent(new BoolType())) {
+      out.bufferCode("cmp $1, %r10");
+    }
+    Instruction trueBlock = i.getNext(1);
+    dfsStack.push(trueBlock);
+    String jmplabel = currlabelMap.get(trueBlock);
+    out.bufferCode("je " + jmplabel);
 
-  public void visit(NopInst i) {
-    //out.bufferCode("// NOP");
+    Instruction falseBlock = i.getNext(0);
+    dfsStack.push(falseBlock);
+
   }
 
-  public void visit(StoreInst i) {}
+  public void visit(LoadInst i) {
+    out.bufferCode("/*** LoadInst ***/");
+    LocalVar dst = i.getDst();
+    AddressVar src = i.getSrcAddress();
 
-  public void visit(ReturnInst i) {}
+    int dstSlot = getStackSlots(dst);
+    int dstOffset = -8 * dstSlot;
+    int srcSlot = getStackSlots(src);
+    int srcOffset = -8 * srcSlot;
+
+    out.bufferCode("movq " + srcOffset + "(%rbp), %r10");
+    out.bufferCode("movq 0(%r10), %r10");
+    out.bufferCode("movq %r10, " + dstOffset + "(%rbp)");
+  }
+
+  public void visit(NopInst i) {
+//    out.bufferCode("// NOP");
+  }
+
+  public void visit(StoreInst i) {
+    out.bufferCode("/*** StoreInst ***/");
+    AddressVar dst = i.getDestAddress();
+    LocalVar src = i.getSrcValue();
+
+    int dstSlot = getStackSlots(dst);
+    int dstOffset = -8 * dstSlot;
+    int srcSlot = getStackSlots(src);
+    int srcOffset = -8 * srcSlot;
+
+    out.bufferCode("movq " + srcOffset + "(%rbp), %r10");
+    out.bufferCode("movq " + dstOffset + "(%rbp), %r11");
+    out.bufferCode("movq %r10, 0(%r11)");
+
+  }
+
+  public void visit(ReturnInst i) {
+    out.bufferCode("/*** ReturnInst ***/");
+    LocalVar retValue = i.getReturnValue();
+    int retSlot = getStackSlots(retValue);
+    int retOffset = -8 * retSlot;
+
+    out.bufferCode("movq " + retOffset + "(%rbp), %rax");
+//    out.bufferCode("leave");
+//    out.bufferCode("ret");
+
+  }
 
   public void moveRegToVar(String reg, Variable var) {
-    int offset = getStackSlots(var) * 8;
-//    out.bufferCode("movq " + reg + ", -" + (offset * 8) + "(%rbp)");
-    out.bufferCode("movq -" + (offset) + "(%rbp), " + reg);
+    int offset = getStackSlots(var) * -8;
+//    out.bufferCode("movq " + reg + ", " + offset + "(%rbp)");
+    out.bufferCode("movq " + offset + "(%rbp), " + reg);
   }
 
   public void visit(CallInst i) {
@@ -233,12 +412,17 @@ public final class CodeGen extends InstVisitor {
       } else {
         int index = argIndex - argRegisters.length;
         out.bufferCode("movq " + (8 * index + 16) + "(%rbp), %r10");
-        moveRegToVar("r10", (Variable) val);
+        moveRegToVar("%r10", (Variable) val);
       }
       ++argIndex;
     }
 
+
     out.bufferCode("call _" + i.getCallee().getName());
+    int dstSlot = getStackSlots(i.getDst());
+    int dstOffset = (-8 * dstSlot);
+    out.bufferCode("movq %rax, " + dstOffset + "(%rbp)");
+
 
   }
 
